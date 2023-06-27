76302d2dbc238653805f766666491f99abf5c28f
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-06-27 16:54:41 +0530

    fixed mobile nav

--- components/global/mobile-nav.tsx
+++ components/global/mobile-nav.tsx
@@ -17,10 +17,10 @@ function MobileNav({ items, children }: MobileNavProps) {
     return (
         <div
             className={cn(
-                "fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden"
+                "fixed inset-0 top-14 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-left-80 md:hidden"
             )}
         >
-            <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
+            <div className="relative z-20 grid gap-6 rounded-md bg-black p-4 text-popover-foreground shadow-md">
                 <Link href="/" className="flex items-center space-x-2">
                     <Icons.music />
                     <span className="font-bold">{siteConfig.name}</span>
