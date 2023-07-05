4f097a660974527864d92cde85a6e8edcd86cd6f
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-07-05 21:18:29 +0530

    updated mobile layout for comments

--- components/blog/blog-view-comments.tsx
+++ components/blog/blog-view-comments.tsx
@@ -14,13 +14,13 @@ function BlogViewComments({ className, comments, users }: PageProps) {
             <div className={className}>
                 {comments.map((comment) => (
                     <div key={comment.id} className="flex gap-4">
-                        <Avatar>
+                        <Avatar className="h-8 w-8 md:w-10 md:h-10">
                             <AvatarImage src={users.find((x) => x.id === comment.authorId)?.profile_image_url!} alt={users.find((x) => x.id === comment.authorId)?.username ?? "User"} />
                             <AvatarFallback>{(users.find((x) => x.id === comment.authorId)?.username ?? "User").charAt(0).toUpperCase()}</AvatarFallback>
                         </Avatar>
                         <div className="w-full space-y-2 cursor-default">
                             <div className="flex gap-2 items-center">
-                                <p>@{users.find((x) => x.id === comment.authorId)?.username}</p>
+                                <p className="text-sm md:text-base">@{users.find((x) => x.id === comment.authorId)?.username}</p>
                                 <p className="text-xs text-gray-500">{convertMstoTimeElapsed(comment.createdAt.getTime())}</p>
                             </div>
                             <p className="font-light text-sm">{comment.content}</p>
