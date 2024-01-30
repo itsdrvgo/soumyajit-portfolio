99f2d01f42ef2aa096baae19ae605c24f6dc6c1e
Sarthak Kundu <thedragoluca@gmail.com>
2024-01-30 15:20:53 +0530

    Remove unused images and update music configuration

--- src/components/home/music.tsx
+++ src/components/home/music.tsx
@@ -1,57 +1,14 @@
 "use client";
 
-import amari_ovyesh from "@/public/amari_ovyesh.jpg";
-import bekhayali_mon from "@/public/bekhayali_mon.jpg";
-import kichu_sopnera from "@/public/kichu_sopnera.jpg";
 import sopne_ba_bastobe from "@/public/sopne_ba_bastobe.png";
-import tor_kothara from "@/public/tor_kothara.jpg";
+import { Music, music } from "@/src/config/music";
 import { cn } from "@/src/lib/utils";
 import { DefaultProps } from "@/src/types";
 import { Divider, Link } from "@nextui-org/react";
-import { HTMLMotionProps, motion, Variants } from "framer-motion";
-import Image, { StaticImageData } from "next/image";
+import { HTMLMotionProps, Variants, motion } from "framer-motion";
+import Image from "next/image";
 import { Icons } from "../icons/icons";
 import SpotifyEmbed from "../ui/spotify-embed";
-
-interface Song {
-    name: string;
-    url: string;
-    thumbnail: StaticImageData;
-    alt: string;
-}
-
-const songs: Song[] = [
-    {
-        name: "Sopne Ba Bastobe",
-        thumbnail: sopne_ba_bastobe,
-        url: "https://open.spotify.com/track/6x2CUb4mZh3nzOaEn9iOdr?si=31dc651e0a614ba1",
-        alt: "sopne_ba_bastobe",
-    },
-    {
-        name: "Amari Ovyesh",
-        thumbnail: amari_ovyesh,
-        url: "https://open.spotify.com/track/2bhO8QYI6dsmM68h4uuf7p?si=c9530c0451f44711",
-        alt: "amari_ovyesh",
-    },
-    {
-        name: "Bekhayali Mon",
-        thumbnail: bekhayali_mon,
-        url: "https://open.spotify.com/track/2NlMIzDlZlTi2X99U3kbO4?si=3a628e382f064d14",
-        alt: "bekhayali_mon",
-    },
-    {
-        name: "Kichu Sopnera",
-        thumbnail: kichu_sopnera,
-        url: "https://open.spotify.com/track/7wtDt3dmEeNjxQMQkMVTGP?si=be2215d7f6e24c1d",
-        alt: "kichu_sopnera",
-    },
-    {
-        name: "Tor Kothara",
-        thumbnail: tor_kothara,
-        url: "https://open.spotify.com/track/7MnWBiI9RGjdHEh4f7bgbm?si=68b8ea9d5d5a400d",
-        alt: "tor_kothara",
-    },
-];
 
 function Music({ className, ...props }: DefaultProps) {
     const slideUpContainer: Variants = {
@@ -106,16 +63,16 @@ function Music({ className, ...props }: DefaultProps) {
                     className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-6"
                     variants={slideUp}
                 >
-                    {songs.slice(0, 1).map((item, index) => (
+                    {music.slice(5, 6).map((item, index) => (
                         <MusicCard song={item} key={index} />
                     ))}
 
                     <div className="grid grid-cols-2 gap-3 md:gap-6">
-                        {songs.slice(1, 4).map((item, index) => (
+                        {music.slice(2, 5).map((item, index) => (
                             <MusicCard song={item} key={index} />
                         ))}
 
-                        <MoreMusicCard song={songs[4]} />
+                        <MoreMusicCard song={music[1]} />
                     </div>
                 </motion.div>
 
@@ -130,7 +87,7 @@ function Music({ className, ...props }: DefaultProps) {
 export default Music;
 
 interface MusicCardProps extends HTMLMotionProps<"div"> {
-    song: Song;
+    song: Music;
 }
 
 function MusicCard({ song, className, ...props }: MusicCardProps) {
@@ -147,7 +104,7 @@ function MusicCard({ song, className, ...props }: MusicCardProps) {
         <motion.div key={song.alt} variants={fadeIn} {...props}>
             <Link
                 className={cn(
-                    "group relative cursor-pointer overflow-hidden rounded-lg",
+                    "group relative size-full cursor-pointer overflow-hidden rounded-lg",
                     className
                 )}
                 color="foreground"
@@ -155,7 +112,11 @@ function MusicCard({ song, className, ...props }: MusicCardProps) {
                 isExternal
             >
                 <Image
-                    src={song.thumbnail}
+                    src={
+                        song.alt === "swapne_ba_bastobe"
+                        ? sopne_ba_bastobe
+                        : song.thumbnail
+                    }
                     alt={song.alt}
                     width={400}
                     height={400}
--- src/config/music.ts
+++ src/config/music.ts
@@ -1,4 +1,4 @@
-interface Music {
+export interface Music {
     name: string;
     url: string;
     thumbnail: string;
